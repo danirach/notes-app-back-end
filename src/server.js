fc753562079ed7b9548af221ae2e310a8f54a706
danirach <danirach96@gmail.com>
2023-01-01 17:42:28 +0700

    restructure with hapi plugin

--- src/server.js
+++ src/server.js
@@ -1,20 +1,30 @@
+/* eslint-disable no-undef */
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
 
-const init = async () =>{
-    const server = Hapi.server({
-        port: 5000,
-        host: 'localhost',
-        routes: {
-          cors: {
-            origin: ['*'],
-          },
-        },
-      });
-    
-    server.route(routes)
-    await server.start()
-    console.log(`server berjalan pada ${server.info.uri}`)
-}
+const init = async () => {
+  const notesService = new NotesService();
+  const server = Hapi.server({
+    port: 5000,
+    host:
+      process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
 
-init()
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
